9f1c0fd8f6819cc99787e7b230268dba42a15f0d
[dyad] <americamahtab@gmail.com>
2026-02-11 04:27:04 +0600

    [dyad] Fixing StoreProvider error in App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductProvider } from "@/context/ProductContext";
 import { CurrencyProvider } from "@/context/CurrencyContext";
+import { StoreProvider } from "./context/StoreContext";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
 import ProductDetails from "./pages/ProductDetails";
@@ -16,18 +17,20 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ProductProvider>
       <CurrencyProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/adminpanel" element={<Admin />} />
-              <Route path="/product/:id" element={<ProductDetails />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
+        <StoreProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/adminpanel" element={<Admin />} />
+                <Route path="/product/:id" element={<ProductDetails />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </TooltipProvider>
+        </StoreProvider>
       </CurrencyProvider>
     </ProductProvider>
   </QueryClientProvider>
